cdccfd0ddfd17d5026656e31b3683123b53fc9bf
voljumet <alexander.laursen@icloud.com>
2021-10-05 15:23:22 +0200

    removed loop skipping

--- connect-4/connect-alex.py
+++ connect-4/connect-alex.py
@@ -55,47 +55,39 @@ X_test = X_test.reshape((X_test.shape[0], 7*6*3))
 # Y_train = Y_train.reshape((Y_train.shape[0], 1))
 # Y_test = Y_test.reshape((Y_test.shape[0], 1))
 
-
-
+f = open("write.csv", "a")
+# resultsss = []
 def kjor(p,j,k,rangez):
     timez_start = time()
     tm = MultiClassTsetlinMachine(p, j, k)
-    # print(f"Accuracy over {rangez} epochs:")
     results = []
     resultat = 0
-    stop_counter = 0
 
     for i in range(rangez):
-        # start_training = time()
         tm.fit(X_train, Y_train, epochs=1, incremental=True)
-        # stop_training = time()
-
-        # start_testing = time()
         result = 100*(tm.predict(X_test) == Y_test).mean()
         if result <= 50:
-            # print("under 50% Accuracy, stopping")
-            stop_counter += 1
             break
         results.append(result)
-        # stop_testing = time()
-        results.sort(reverse=True)
         resultat = results[0]
+
     if resultat != 0:
+        results.sort(reverse=True)
         timez_stop = time()
         print(f"T: {j}, s: {k}, #c: {p}")
+        f.write(f"\n{j},{k},{p},{resultat}")
         print(f"Best Accuracy over {rangez} epochs: {resultat}, time: {timez_stop-timez_start}")
                                                                        # , stop_training-start_training, stop_testing-start_testing))
-    return stop_counter
-
 # np.random.shuffle(training_dataset)
 for i in range(100, 3000, 100):
-    rezz = 0
+    # rezz = 0
     for j in range(10, 200, 5):
-        res = 0
-        if rezz == 3:
-            break
+        # res = 0
+        # if rezz == 3:
+        #     break
         for k in range(3, 100, 3):
-            res += kjor(i, j, k, 6)
-            if res == 3:
-                rezz += 1
-                break
+            # res += \
+            kjor(i, j, k, 9)
+            # if res == 3:
+            #     rezz += 1
+            #     break
