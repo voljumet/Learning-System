536a53111b8a9749244944c040479b4c635fa500
voljumet <alexander.laursen@icloud.com>
2021-11-20 17:43:52 +0100

    fix comments and input loop

--- Go-Tsetlin/Cuda-go-go-go.py
+++ Go-Tsetlin/Cuda-go-go-go.py
@@ -16,37 +16,45 @@ def store_as_data(data, filename):
 
 
 def load_data(filename):
-	inputFile = filename+'.data'
-	fd = open(inputFile, 'rb')
+	input_file = filename+'.data'
+	fd = open(input_file, 'rb')
 	return pickle.load(fd)
 
 
 ''' ----------------- SETTINGS ----------------------------------- '''
 game_size = 9
-sgf_dir = 'go9-large'
+sgf_dir = 'go9'
 
-#Use "win", "black", or "white" to pick prediction model.
+# Use "win", "black", or "white" to pick prediction model.
 type_predict = "black"
+inn = ''
 
 # True = Load data from folders and save as data-file.
 # False = Load data from data-file.
-newData = True
-''' ---------------------------------------------------------------------------- '''
+while True:
+	inn = input("Load data from folder = T, from datafile = F: ")
+	if inn == "T":
+		newData = True
+		break
+	elif inn == "F":
+		newData = False
+		break
+''' -------------------------------------------------------------- '''
 
 if newData:
-	print("Dataset is being loaded ... \n")
+	print("Dataset is being loaded from folder... \n")
 	X_linear, Y_temp = create_TM_representations(sgf_dir, game_size, type_predict)
 	print("Load complete ... \n")
 
 	print("Reshaping from linear to matrix ... \n")
 	X_temp = X_linear[:, :].reshape(X_linear.shape[0], 9, 18)
-	print("Reshap complete ... \n")
+	print("Reshape complete ... \n")
 
-	print("Saving data as files ... \n")
+	print("Saving as data-files ... \n")
 	store_as_data(X_temp, "X_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 	store_as_data(Y_temp, "Y_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 else:
-	print("Using data from file ... \n")
+	print("Skipping loading from folder, using data-files ... \n")
 
 
 X_temp = load_data("X_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
@@ -55,19 +63,20 @@ Y_temp = load_data("Y_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 # split into test / train and shuffle
 X_train, X_test, Y_train, Y_test = train_test_split(X_temp, Y_temp, test_size=0.33, random_state=42, shuffle=True)
 
+''' ----------------- SETTINGS ----------------------------------- '''
 clauses = 200
 Threshold = 200
 Forget_rate = 50
+epochs = 100
 ctm = MultiClassConvolutionalTsetlinMachine2D(number_of_clauses=clauses, T=Threshold, s=Forget_rate, patch_dim=(9, 18), max_weight=16)
 
 f = open("log.txt", "a")
-f.write(f"Type of predicting: {type_predict}, \n"+f"Number_of_clauses = {clauses}, "+ f"T = {Threshold}, "+ f"S = {Forget_rate}" + "\n\n")
+f.write(f"Predict: {type_predict}, "+f"Number_of_clauses = {clauses}, "+ f"T = {Threshold}, "+ f"S = {Forget_rate}"+ f"Epocs = {epochs}" + "\n")
 f.close()
 
 
 results = np.zeros(0)
 print("Training...")
-epochs = 100
 for i in range(100):
 	start = time()
 	ctm.fit(X_train, Y_train, epochs=epochs)
--- Go-Tsetlin/Go-go-go-go.py
+++ Go-Tsetlin/Go-go-go-go.py
@@ -9,41 +9,52 @@ from pyTsetlinMachine.tm import MultiClassConvolutionalTsetlinMachine2D
 from process_go_ds import create_TM_representations
 
 
-game_size = 9  # determines number of columns and rows in the game, this must match with the dataset we are loading
-# sgf_dir = '../go9-large'  # here you speciy your Go dataset sgf files directory
-sgf_dir = 'go9-large'  # here you speciy your Go dataset sgf files directory
-
 def store_as_data(data, filename):
 	fw = open(filename+'.data', 'wb')
 	pickle.dump(data, fw)
 	fw.close()
 
+
 def load_data(filename):
-	inputFile = filename+'.data'
-	fd = open(inputFile, 'rb')
+	input_file = filename+'.data'
+	fd = open(input_file, 'rb')
 	return pickle.load(fd)
 
 
-''' 
-Use "win", "black", or "white" to pick prediction model.
-'''
+''' ----------------- SETTINGS ----------------------------------- '''
+game_size = 9
+sgf_dir = 'go9-large'
+
+# Use "win", "black", or "white" to pick prediction model.
 type_predict = "black"
-newData = True
+inn = ''
+
+# True = Load data from folders and save as data-file.
+# False = Load data from data-file.
+while True:
+	inn = input("Load data from folder = T, from datafile = F: ")
+	if inn == "T":
+		newData = True
+		break
+	elif inn == "F":
+		newData = False
+		break
+''' -------------------------------------------------------------- '''
 
 if newData:
-	print("Dataset is being loaded ... \n")
+	print("Dataset is being loaded from folder... \n")
 	X_linear, Y_temp = create_TM_representations(sgf_dir, game_size, type_predict)
 	print("Load complete ... \n")
 
 	print("Reshaping from linear to matrix ... \n")
 	X_temp = X_linear[:, :].reshape(X_linear.shape[0], 9, 18)
-	print("Reshap complete ... \n")
+	print("Reshape complete ... \n")
 
-	print("Saving data as files ... \n")
+	print("Saving as data-files ... \n")
 	store_as_data(X_temp, "X_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 	store_as_data(Y_temp, "Y_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 else:
-	print("Skipping data loading ... \n")
+	print("Skipping loading from folder, using data-files ... \n")
 
 
 X_temp = load_data("X_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
@@ -52,19 +63,21 @@ Y_temp = load_data("Y_temp_"+type_predict+"_"+sgf_dir+"_"+str(game_size))
 # split into test / train and shuffle
 X_train, X_test, Y_train, Y_test = train_test_split(X_temp, Y_temp, test_size=0.33, random_state=42, shuffle=True)
 
+''' ------------- SETTINGS ------------- '''
 clauses = 200
 Threshold = 200
 Forget_rate = 50
+epochs = 100
+''' ------------------------------------ '''
 ctm = MultiClassConvolutionalTsetlinMachine2D(number_of_clauses=clauses, T=Threshold, s=Forget_rate, patch_dim=(9, 18), boost_true_positive_feedback=0)
 
 f = open("log.txt", "a")
-f.write(f"Type of predicting: {type_predict}, \n"+f"Number_of_clauses = {N_clauses}, "+ f"T = {Threshold}, "+ f"S = {Forget_rate}" + "\n\n")
+f.write(f"Predict: {type_predict}, "+f"Number_of_clauses = {clauses}, "+ f"T = {Threshold}, "+ f"S = {Forget_rate}"+ f"Epocs = {epochs}" + "\n")
 f.close()
 
 
 results = np.zeros(0)
 print("Training...")
-epochs = 100
 for i in range(100):
 	start = time()
 	ctm.fit(X_train, Y_train, epochs=epochs)
