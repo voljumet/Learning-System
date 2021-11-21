05e8ecc9bee29d096aa0648b6dbe6944ca9e9433
voljumet <alexander.laursen@icloud.com>
2021-11-21 23:22:26 +0100

    added process

--- Go-Tsetlin/process_go_ds.py
+++ Go-Tsetlin/process_go_ds.py
@@ -42,7 +42,7 @@ def convert_game_to_bits(stone_positions, game_size):
     return bits  # all bits representations for both players are concatenated to create the training set X
 
 
-def convert_game_to_bits_2(x_temp, game_size, function_input):
+def convert_game_to_bits_2(x_temp, game_size, function_input, moves_to_predict):
 
     # bits = np.zeros(game_size**2 *2)  # because 9 x 9 is 9**2, and we double that since we will represent each stone or empty intersection with 2 bits
     game_size_alphabets = alphabet[:game_size]  # if game size is 9, it has 9 elements
@@ -68,10 +68,36 @@ def convert_game_to_bits_2(x_temp, game_size, function_input):
     if x_temp[:-1][0] == remove_last:
         x_temp.pop()
 
-    x_array, y_array = recursive(x_temp, go_flatten_board, go_flatten_board_small, game_size, x_array, y_array)
+    if moves_to_predict == 0:
+        x_array, y_array = recursive(x_temp, go_flatten_board, go_flatten_board_small, game_size, x_array, y_array)
+    else:
+        x_array, y_array = predict_second_move(x_temp, go_flatten_board, go_flatten_board_small, game_size, function_input, moves_to_predict)
 
     return x_array, y_array
 
+def predict_second_move(x_temp, go_flatten_board, go_flatten_board_small, game_size, function_input, moves_to_predict):
+    bits = np.zeros(game_size ** 2 * 2)
+
+
+
+    if function_input == "black":
+        moves_to_predict -= 1
+
+    if len(x_temp) != 2:  # stop the recursion if there are no more moves
+        for move in x_temp[:moves_to_predict]:
+            if move[0] == 'B':  # check if the position for black stone
+                first, second = 1, 0
+            elif move[0] == 'W':  # check if the position for white stone
+                first, second = 0, 1
+
+            ids = move[2:4].strip(']')  # extracts only row_id and columns_id from the string
+            if re.match('[a-zA-Z]+', ids):
+                bits[go_flatten_board.index(ids + "0")] = first
+                bits[go_flatten_board.index(ids + "1")] = second
+
+        label = go_flatten_board_small.index(x_temp[moves_to_predict][2:4])
+
+    return bits, label
 
 def recursive(x_temp, go_flatten_board, go_flatten_board_small, game_size, x_result=None, y_result=None):
     bits = np.zeros(game_size ** 2 * 2)
@@ -100,7 +126,7 @@ def recursive(x_temp, go_flatten_board, go_flatten_board_small, game_size, x_res
     return x_result, y_result
 
 
-def create_TM_representations(dataset_dir, game_size, function_input):
+def create_TM_representations(dataset_dir, game_size, function_input, moves_to_predict):
     ''' we loop through all sgf files we have and read each file (game sample stones positions with winner label) '''
     X = []
     Y = []
@@ -138,11 +164,17 @@ def create_TM_representations(dataset_dir, game_size, function_input):
                                 x_temp.append(p)
 
                     if len(x_temp) > 2:
-                        stone_bits_representation, label = convert_game_to_bits_2(x_temp, game_size, function_input)
-                        for i, each in enumerate(stone_bits_representation):
-                            X.append(each)
-                            Y.append(label[i])
-
+                        if moves_to_predict == 0:
+                            stone_bits_representation, label = convert_game_to_bits_2(x_temp, game_size, function_input,
+                                                                                      moves_to_predict)
+                            for i, each in enumerate(stone_bits_representation):
+                                X.append(each)
+                                Y.append(label[i])
+                        else:
+                            stone_bits_representation, label = convert_game_to_bits_2(x_temp, game_size, function_input,
+                                                                                      moves_to_predict)
+                            X.append(stone_bits_representation)
+                            Y.append(label)
 
     print("Games with win: ", win)
     print("Games with loss: ", loss)
